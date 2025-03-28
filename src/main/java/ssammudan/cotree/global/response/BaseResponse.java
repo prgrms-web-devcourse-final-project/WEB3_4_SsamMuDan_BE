d19d7ec754b4f5e36d21a5cff64f111e3bfb24a3
KangBaekGwa <ksu9801@gmail.com>
2025-03-28 16:37:42 +0900

    refactor : BaseResponse 수정 및 AOP 추가 - BaseResponse, data 항목 null 이라도 반환하도록 변경 - Controller Layer 응답 시, BaseResponse 의 Httpstatus 에 따라 status 변경되도록 AOP 적용

--- src/main/java/ssammudan/cotree/global/response/BaseResponse.java
+++ src/main/java/ssammudan/cotree/global/response/BaseResponse.java
@@ -1,7 +1,9 @@
 package ssammudan.cotree.global.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -9,51 +11,56 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@JsonInclude(Include.NON_NULL)
 @Builder(access = AccessLevel.PRIVATE)
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class BaseResponse<T> {
 
-    @NotNull
-    private final Boolean isSuccess;
-    @NotNull
-    private final String  code;
-    @NotNull
-    private final String  message;
-    private final T       data;
+	@JsonIgnore
+	private final HttpStatus httpStatus;
+	@NotNull
+	private final Boolean isSuccess;
+	@NotNull
+	private final String code;
+	@NotNull
+	private final String message;
+	private final T data;
 
-    public static <T> BaseResponse<T> success(final SuccessCode successCode, final T data) {
-        return BaseResponse.<T>builder()
-                           .isSuccess(true)
-                           .code(successCode.getCode())
-                           .message(successCode.getMessage())
-                           .data(data)
-                           .build();
-    }
+	public static <T> BaseResponse<T> success(final SuccessCode successCode, final T data) {
+		return BaseResponse.<T>builder()
+				.httpStatus(successCode.getStatus())
+				.isSuccess(true)
+				.code(successCode.getCode())
+				.message(successCode.getMessage())
+				.data(data)
+				.build();
+	}
 
-    public static <T> BaseResponse<T> success(final SuccessCode successCode) {
-        return BaseResponse.<T>builder()
-                           .isSuccess(true)
-                           .code(successCode.getCode())
-                           .message(successCode.getMessage())
-                           .data(null)
-                           .build();
-    }
+	public static <T> BaseResponse<T> success(final SuccessCode successCode) {
+		return BaseResponse.<T>builder()
+				.httpStatus(successCode.getStatus())
+				.isSuccess(true)
+				.code(successCode.getCode())
+				.message(successCode.getMessage())
+				.data(null)
+				.build();
+	}
 
-    public static <T> BaseResponse<T> fail(final ErrorCode errorCode) {
-        return BaseResponse.<T>builder()
-                           .isSuccess(true)
-                           .code(errorCode.getCode())
-                           .message(errorCode.getMessage())
-                           .build();
-    }
+	public static <T> BaseResponse<T> fail(final ErrorCode errorCode) {
+		return BaseResponse.<T>builder()
+				.httpStatus(errorCode.getStatus())
+				.isSuccess(false)
+				.code(errorCode.getCode())
+				.message(errorCode.getMessage())
+				.build();
+	}
 
-    public static <T> BaseResponse<T> fail(final ErrorCode errorCode, final String message) {
-        return BaseResponse.<T>builder()
-                           .isSuccess(true)
-                           .code(errorCode.getCode())
-                           .message(message)
-                           .build();
-    }
+	public static <T> BaseResponse<T> fail(final ErrorCode errorCode, final String message) {
+		return BaseResponse.<T>builder()
+				.httpStatus(errorCode.getStatus())
+				.isSuccess(false)
+				.code(errorCode.getCode())
+				.message(message)
+				.build();
+	}
 
 }
