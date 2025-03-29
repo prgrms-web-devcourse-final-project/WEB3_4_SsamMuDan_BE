9eb512c0bfde69b92ed5f0b099aadf9198993944
KangBaekGwa <ksu9801@gmail.com>
2025-03-30 00:32:26 +0900

    feat : Entity 별도 생성으로 기존 Entity 삭제

--- src/main/java/ssammudan/cotree/model/member/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package ssammudan.cotree.model.member.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ssammudan.cotree.model.member.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
