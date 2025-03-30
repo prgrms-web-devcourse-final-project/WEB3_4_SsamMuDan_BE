b594bc36451d6aa21710f9bfb1a3d6ecde50c300
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-31 00:02:29 +0900

    fix: 엔티티 컬럼 수정에 따른 DDL 추가

--- src/main/resources/db/migration/V1.2__modify_time_to_bigint.sql
+++
@@ -1,6 +0,0 @@
-# Date :            2025-03-30
-# Author :          loadingKKamo21
-# Description :     영상 길이 컬럼 타입 TIME에서 BIGINT로 변경
-
--- Modify tech_tube duration column time to bigint
-ALTER TABLE tech_tube MODIFY COLUMN tech_tube_duration BIGINT UNSIGNED;
