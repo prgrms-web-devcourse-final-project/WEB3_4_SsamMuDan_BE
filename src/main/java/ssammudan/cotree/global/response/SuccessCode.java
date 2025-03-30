439665610d2d2f2aef3ab305239f7885e67e4814
sobaman <sobaman522@gmail.com>
2025-03-30 18:44:12 +0900

    feat: 채용 도메인 성공 코드 추가
    - 이력서 작성시 성공코드 추가

--- src/main/java/ssammudan/cotree/global/response/SuccessCode.java
+++ src/main/java/ssammudan/cotree/global/response/SuccessCode.java
@@ -1,37 +1,39 @@
 package ssammudan.cotree.global.response;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessCode {
 
-    //Member
+	//Member
 
-    //Education
+	//Education
 
-    //Payment
+	//Payment
 
-    //Recruitment
+	//Recruitment
+	RESUME_CREATE_SUCCESS(HttpStatus.CREATED, "201", "이력서 작성을 성공하였습니다.")
 
-    //Project
+	//Project
 
-    //Community
+	//Community
 
-    //${}: 6001 ~ 7000
+	//${}: 6001 ~ 7000
 
-    //${}: 7001 ~ 8000
+	//${}: 7001 ~ 8000
 
-    //${}: 8001 ~ 9000
+	//${}: 8001 ~ 9000
 
-    //Common: 9001 ~ 9999
-    ;
+	//Common: 9001 ~ 9999
+	;
 
-    private final HttpStatus status;
-    private final String     code;
-    private final String     message;
+	private final HttpStatus status;
+	private final String code;
+	private final String message;
 
 }
