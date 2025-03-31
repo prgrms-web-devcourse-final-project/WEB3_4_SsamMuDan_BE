8328f67f50a32e776c23ceaaf41055a513a2961b
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-31 16:55:04 +0900

    refactor: SuccessCode 메시지 수정

--- src/main/java/ssammudan/cotree/global/response/SuccessCode.java
+++ src/main/java/ssammudan/cotree/global/response/SuccessCode.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
@@ -15,24 +14,24 @@ public enum SuccessCode {
 	MEMBER_CREATE_SUCCESS(HttpStatus.CREATED, "201", "회원가입을 완료했습니다."),
 
 	//Education
-	TECH_BOOK_READ_SUCCESS(HttpStatus.OK, "200", "TechBook을 성공적으로 불러왔습니다."),
+	TECH_BOOK_READ_SUCCESS(HttpStatus.OK, "200", "TechBook 조회를 완료했습니다."),
 
-    //Payment
+	//Payment
 
-    //Recruitment
+	//Recruitment
 
-    //Project
+	//Project
 
-    //Community
+	//Community
 
-    //${}: 6001 ~ 7000
+	//${}: 6001 ~ 7000
 
-    //${}: 7001 ~ 8000
+	//${}: 7001 ~ 8000
 
-    //${}: 8001 ~ 9000
+	//${}: 8001 ~ 9000
 
-    //Common: 9001 ~ 9999
-    ;
+	//Common: 9001 ~ 9999
+	;
 
 	private final HttpStatus status;
 	private final String code;
