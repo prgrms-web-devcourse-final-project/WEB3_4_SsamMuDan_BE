115ebc0d2dd085c021b7d939a3a0daa505abce1d
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-03-31 16:25:48 +0900

    feat: EducationlevelRepository 메서드 추가
    - findByNameIgnoreCase(): 명칭 일치로 단 건 조회
    - findAllByNameContainsIgnoreCase(): 명칭 포함으로 다 건 조회

--- src/main/java/ssammudan/cotree/model/education/level/repository/EducationLevelRepository.java
+++ src/main/java/ssammudan/cotree/model/education/level/repository/EducationLevelRepository.java
@@ -1,5 +1,8 @@
 package ssammudan.cotree.model.education.level.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ssammudan.cotree.model.education.level.entity.EducationLevel;
@@ -16,4 +19,9 @@ import ssammudan.cotree.model.education.level.entity.EducationLevel;
  * 25. 3. 29.    loadingKKamo21       Initial creation
  */
 public interface EducationLevelRepository extends JpaRepository<EducationLevel, Long> {
+
+	Optional<EducationLevel> findByNameIgnoreCase(String name);
+
+	List<EducationLevel> findAllByNameContainsIgnoreCase(String name);
+
 }
