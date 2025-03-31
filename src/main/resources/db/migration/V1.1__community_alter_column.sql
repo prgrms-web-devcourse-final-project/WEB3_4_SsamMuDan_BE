d972452ec1d5e3785ba58cc12b44215fab27cab2
KangBaekGwa <ksu9801@gmail.com>
2025-03-31 11:32:02 +0900

    remove : 필요없는 migration file 삭제

--- src/main/resources/db/migration/V1.1__community_alter_column.sql
+++
@@ -1,6 +0,0 @@
-# Date :            2025-03-28
-# Author :          Baekgwa
-# Description :     커뮤니티 Table `view_count` Column int->bigint 로 변환. 사유 : viewcount 는 많이 증가할 수 있어, 오버플로우 발생할 수 있을 것 같음.
-
-ALTER TABLE community
-    MODIFY COLUMN view_count bigint DEFAULT 0 NOT NULL;
