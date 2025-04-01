f6baaf3f33727f18bdfb9f4b96051aa7128286fe
sobaman <sobaman522@gmail.com>
2025-04-02 00:13:26 +0900

    refactor: 배치 사이즈 수정

--- src/main/java/ssammudan/cotree/model/recruitment/resume/resume/repository/ResumeRepositoryImpl.java
+++ src/main/java/ssammudan/cotree/model/recruitment/resume/resume/repository/ResumeRepositoryImpl.java
@@ -27,27 +27,37 @@ public class ResumeRepositoryImpl implements ResumeRepositoryCustom {
 
 	private final JdbcTemplate jdbcTemplate;
 	private static final String UPDATE_VIEW_COUNT_SQL = "UPDATE resume SET view_count = view_count + ? WHERE id = ?";
+	private static final int MAX_BATCH_SIZE = 300;
 
 	@Override
 	public void bulkUpdateViewCount(Map<Long, Integer> viewCountData) {
 
 		List<Map.Entry<Long, Integer>> entries = new ArrayList<>(viewCountData.entrySet());
+		int totalSize = entries.size();
+		int batchCount = (totalSize + MAX_BATCH_SIZE - 1) / MAX_BATCH_SIZE;
 
-		jdbcTemplate.batchUpdate(
-			UPDATE_VIEW_COUNT_SQL,
-			new BatchPreparedStatementSetter() {
-				@Override
-				public void setValues(PreparedStatement ps, int i) throws SQLException {
-					Map.Entry<Long, Integer> entry = entries.get(i);
-					ps.setInt(1, entry.getValue());
-					ps.setLong(2, entry.getKey());
-				}
+		for (int i = 0; i < batchCount; i++) {
+			int fromIndex = i * MAX_BATCH_SIZE;
+			int toIndex = Math.min((i + 1) * MAX_BATCH_SIZE, totalSize);
 
-				@Override
-				public int getBatchSize() {
-					return 300;
+			final List<Map.Entry<Long, Integer>> batchEntries = entries.subList(fromIndex, toIndex);
+
+			jdbcTemplate.batchUpdate(
+				UPDATE_VIEW_COUNT_SQL,
+				new BatchPreparedStatementSetter() {
+					@Override
+					public void setValues(PreparedStatement ps, int i) throws SQLException {
+						Map.Entry<Long, Integer> entry = batchEntries.get(i);
+						ps.setInt(1, entry.getValue());
+						ps.setLong(2, entry.getKey());
+					}
+
+					@Override
+					public int getBatchSize() {
+						return batchEntries.size();
+					}
 				}
-			}
-		);
+			);
+		}
 	}
 }
