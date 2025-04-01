e89636b37a16c214a51c425b47c6cd82815958e8
k-haechan <gibbm1127@naver.com>
2025-04-01 14:41:11 +0900

    feat: Spring Security 기능 추가 및 인증/인가 기능 구현 - Spring Security 설정 파일 추가 - 사용자 인증 및 권한 관리 기능 구현 - JWT 기반 인증 시스템 추가 - 기본적인 보안 필터링 및 경로 권한 설정 적용

---
+++ src/main/java/ssammudan/cotree/global/config/security/user/CustomUserDetailsService.java
@@ -0,0 +1,60 @@
+package ssammudan.cotree.global.config.security.user;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import ssammudan.cotree.global.config.security.jwt.AccessTokenService;
+import ssammudan.cotree.global.config.security.jwt.RefreshTokenService;
+import ssammudan.cotree.global.error.GlobalException;
+import ssammudan.cotree.global.response.ErrorCode;
+import ssammudan.cotree.model.member.member.entity.Member;
+import ssammudan.cotree.model.member.member.repository.MemberRepository;
+
+/**
+ * PackageName : ssammudan.cotree.global.config.security.user
+ * FileName    : CustomUserDetailsService
+ * Author      : hc
+ * Date        : 25. 3. 31.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 3. 31.     hc               Initial creation
+ */
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+	private final MemberRepository memberRepository;
+	private final AccessTokenService accessTokenService;
+	private final RefreshTokenService refreshTokenService;
+
+	@Override
+	public CustomUser loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member member = memberRepository.findByUsername(username)
+			.orElseThrow(() -> new GlobalException(ErrorCode.MEMBER_NOT_FOUND));
+		return new CustomUser(member, null);
+	}
+
+	public CustomUser loadUserById(String id) throws UsernameNotFoundException {
+		Member member = memberRepository.findById(id)
+			.orElseThrow(() -> new GlobalException(ErrorCode.MEMBER_NOT_FOUND));
+		// new UsernameNotFoundException("id에 맞는 멤버가 존재하지 않습니다.");
+		return new CustomUser(member, null);
+	}
+
+	public CustomUser loadUserByAccessToken(String accessToken) {
+		try {
+			Claims claimsFromToken = accessTokenService.getClaimsFromToken(accessToken);
+			String id = claimsFromToken.getId();
+			Member member = memberRepository.findById(id)
+				.orElseThrow(() -> new GlobalException(ErrorCode.MEMBER_NOT_FOUND));
+			return new CustomUser(member, null);
+		} catch (Exception e) { // Jwt는 굳이 예외처리하지 않음. 이 후 필터로 전달하기 위해
+			return null;
+		}
+	}
+
+}
