349a9c11b243d5f2d89a399f96e1763018e20b3a
KangBaekGwa <ksu9801@gmail.com>
2025-04-01 15:17:08 +0900

    feat : member, community, like, comment `dev` 프로파일용, 더미 데이터 생성

---
+++ src/main/resources/db/seed/dev/R__D_insert_like_seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO likes (member_id, tech_tube_id, tech_book_id, project_id, community_id, created_at, modified_at)
+VALUES (1, NULL, NULL, NULL, 1, NOW(), NOW()),
+       (1, NULL, NULL, NULL, 2, NOW(), NOW()),
+       (1, NULL, NULL, NULL, 3, NOW(), NOW()),
+       (1, NULL, NULL, NULL, 4, NOW(), NOW()),
+       (1, NULL, NULL, NULL, 5, NOW(), NOW()),
+       (2, NULL, NULL, NULL, 1, NOW(), NOW()),
+       (2, NULL, NULL, NULL, 2, NOW(), NOW()),
+       (2, NULL, NULL, NULL, 3, NOW(), NOW()),
+       (2, NULL, NULL, NULL, 4, NOW(), NOW()),
+       (2, NULL, NULL, NULL, 5, NOW(), NOW())
