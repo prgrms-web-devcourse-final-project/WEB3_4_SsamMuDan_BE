362107494a033798d3d3e7a6c48704d41075178b
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-02 16:59:02 +0900

    test: 테스트 코드 todo 리스트 작성

---
+++ src/test/java/ssammudan/cotree/domain/project/service/ProjectServiceImplTest.java
@@ -0,0 +1,16 @@
+package ssammudan.cotree.domain.project.service;
+
+/**
+ * PackageName : ssammudan.cotree.domain.project.service
+ * FileName    : ProjectServiceImplTest
+ * Author      : sangxxjin
+ * Date        : 2025. 4. 2.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 4. 2.     sangxxjin               Initial creation
+ */
+class ProjectServiceImplTest {
+	//todo: 개발 예정
+}
