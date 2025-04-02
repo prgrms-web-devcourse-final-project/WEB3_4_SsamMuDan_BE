fbb6ae20aa3e37edfb772edcf7dbaeee3c550599
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-02 16:55:33 +0900

    feat: create project controller 구현 - 이미지 파일의 경우 request로 같이 처리가 안돼 request에서 필요한 정보 받고 파일은 따로 처리하게 구현 했습니다.

---
+++ src/main/java/ssammudan/cotree/domain/project/controller/ProjectController.java
@@ -0,0 +1,49 @@
+package ssammudan.cotree.domain.project.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ssammudan.cotree.domain.project.dto.ProjectCreateRequest;
+import ssammudan.cotree.domain.project.dto.ProjectCreateResponse;
+import ssammudan.cotree.domain.project.service.ProjectServiceImpl;
+import ssammudan.cotree.global.response.BaseResponse;
+import ssammudan.cotree.global.response.SuccessCode;
+
+/**
+ * PackageName : ssammudan.cotree.domain.project.controller
+ * FileName    : ProjectController
+ * Author      : sangxxjin
+ * Date        : 2025. 4. 2.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 4. 2.     sangxxjin               Initial creation
+ */
+@RestController
+@RequestMapping("/api/v1/project")
+@RequiredArgsConstructor
+@Tag(name = "Proejct Controller", description = "프로젝트 생성 API")
+public class ProjectController {
+	private final ProjectServiceImpl projectServiceImpl;
+
+	@PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@Operation(summary = "프로젝트 생성")
+	public BaseResponse<ProjectCreateResponse> createProject(
+		@RequestPart("request") @Valid ProjectCreateRequest request,
+		@RequestPart(value = "projectImageUrl", required = false) MultipartFile projectImage) {
+		//todo: 현재 회원 정보 하드 코딩 -> 로그인한 member 넘기게 수정 예정
+		ProjectCreateResponse response = projectServiceImpl.create(request, projectImage, "1");
+
+		return BaseResponse.success(SuccessCode.PROJECT_CREATE_SUCCESS, response);
+	}
+}
+
