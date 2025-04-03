e873c4cd316d3fa35ec9217b7fc3a7fffb4f53c7
k-haechan <gibbm1127@naver.com>
2025-04-03 16:17:35 +0900

    feat : SpringSecurity를 이용한 인증 객체 생성 - OAuth2UserService - OAuth2SuccessHandler - OAuth2FailureHandler

--- src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
+++ src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
@@ -22,6 +22,9 @@ import lombok.RequiredArgsConstructor;
 import ssammudan.cotree.global.config.security.exception.CustomAccessDeniedHandler;
 import ssammudan.cotree.global.config.security.exception.CustomAuthenticationEntryPoint;
 import ssammudan.cotree.global.config.security.filter.JwtAuthenticationFilter;
+import ssammudan.cotree.global.config.security.oauth.OAuth2UserService;
+import ssammudan.cotree.global.config.security.oauth.handler.OAuth2FailureHandler;
+import ssammudan.cotree.global.config.security.oauth.handler.OAuth2SuccessHandler;
 import ssammudan.cotree.infra.frontend.FrontendConfig;
 
 @Configuration
@@ -32,6 +35,9 @@ public class SecurityConfig {
 	private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 	private final CustomAccessDeniedHandler accessDeniedHandler;
 	private final FrontendConfig frontendConfig;
+	private final OAuth2UserService oAuth2UserService;
+	private final OAuth2SuccessHandler oAuth2SuccessHandler;
+	private final OAuth2FailureHandler oAuth2FailureHandler;
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -42,66 +48,73 @@ public class SecurityConfig {
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http
-				// ✅ 보안 관련 설정 (CSRF, CORS, 세션)
-				.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화 (JWT 사용 시 불필요)
-				.cors(cors -> corsConfigurationSource()) // CORS 설정 적용
-				.sessionManagement(
-						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안함 (JWT 방식)
+			// ✅ 보안 관련 설정 (CSRF, CORS, 세션)
+			.csrf(AbstractHttpConfigurer::disable) // CSRF 비활성화 (JWT 사용 시 불필요)
+			.cors(cors -> corsConfigurationSource()) // CORS 설정 적용
+			.sessionManagement(
+				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 사용 안함 (JWT 방식)
 
-				// ✅ 필터 설정 (JWT 인증 필터 → UsernamePasswordAuthenticationFilter)
-				.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			// ✅ 필터 설정 (JWT 인증 필터 → UsernamePasswordAuthenticationFilter)
+			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 
-				// ✅ 인증 및 접근 권한 설정
-				.authorizeHttpRequests(authorize -> authorize
-						// H2 콘솔 허용
-						.requestMatchers("/h2-console/**").permitAll()
-						// 프론트엔드에서 적용될 예외 포인트 설정
-						.requestMatchers("/error", "/favicon.ico").permitAll()
-						// Swagger 문서 접근 허용
-						.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
-								"/swagger-ui.html").permitAll()
-						.requestMatchers(GET, "/actuator/health").permitAll()
+			// ✅ 인증 및 접근 권한 설정
+			.authorizeHttpRequests(authorize -> authorize
+				// H2 콘솔 허용
+				.requestMatchers("/h2-console/**").permitAll()
+				// 프론트엔드에서 적용될 예외 포인트 설정
+				.requestMatchers("/error", "/favicon.ico").permitAll()
+				// Swagger 문서 접근 허용
+				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
+					"/swagger-ui.html").permitAll()
+				.requestMatchers(GET, "/actuator/health").permitAll()
 
-						// MEMBER Domain
-						.requestMatchers(POST, "/api/v1/member/signup").permitAll()
-						.requestMatchers(POST, "/api/v1/member/signin").permitAll()
+				// MEMBER Domain
+				.requestMatchers(POST, "/api/v1/member/signup").permitAll()
+				.requestMatchers(POST, "/api/v1/member/signin").permitAll()
 
-						// Category Domain
-						.requestMatchers(GET, "/api/v1/category/**").permitAll()
+				// Category Domain
+				.requestMatchers(GET, "/api/v1/category/**").permitAll()
 
-						// Comment Domain
-						// 해당 없음. 모두 인증 필요
+				// Comment Domain
+				// 해당 없음. 모두 인증 필요
 
-						// Community Domain
-						.requestMatchers(GET, "/api/v1/community/board").permitAll()
-						.requestMatchers(GET, "/api/v1/community/board/**").permitAll()
+				// Community Domain
+				.requestMatchers(GET, "/api/v1/community/board").permitAll()
+				.requestMatchers(GET, "/api/v1/community/board/**").permitAll()
 
-						// Education / TechBook Domain
-						.requestMatchers(GET, "/api/v1/education/techbook/**").permitAll()
-						.requestMatchers(GET, "/api/v1/education/techbook").permitAll()
+				// Education / TechBook Domain
+				.requestMatchers(GET, "/api/v1/education/techbook/**").permitAll()
+				.requestMatchers(GET, "/api/v1/education/techbook").permitAll()
 
-						// Education / TechTube Domain
-						// 해당 없음. 모두 인증 필요
+				// Education / TechTube Domain
+				// 해당 없음. 모두 인증 필요
 
-						// Resume Domain
-						// .requestMatchers(GET, "/api/v1/recruitment/resume/**").permitAll()
+				// Resume Domain
+				// .requestMatchers(GET, "/api/v1/recruitment/resume/**").permitAll()
 
-						// Project Domain
-						// 해당 없음. 모두 인증 필요
+				// Project Domain
+				// 해당 없음. 모두 인증 필요
 
-						// Upload Domain
-						// 해당 없음. 모두 인증 필요
+				// Upload Domain
+				// 해당 없음. 모두 인증 필요
 
-						.anyRequest().authenticated()) // 그 외 요청은 인증 필요
+				.anyRequest().authenticated()) // 그 외 요청은 인증 필요
+			// ✅ OAuth2 로그인 설정
+			.oauth2Login(oauth2 -> oauth2
+				.userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+					.userService(oAuth2UserService)) // 사용자 정보 서비스 설정
+				.successHandler(oAuth2SuccessHandler) // OAuth2 로그인 성공 핸들러
+				.failureHandler(oAuth2FailureHandler) // OAuth2 로그인 실패 핸들러
+			)
 
-				// ✅ 기본 인증 방식 비활성화 (JWT 사용)
-				.httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
-				.formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
+			// ✅ 기본 인증 방식 비활성화 (JWT 사용)
+			.httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 인증 비활성화
+			.formLogin(AbstractHttpConfigurer::disable) // 폼 로그인 비활성화
 
-				// ✅ 예외 처리 설정
-				.exceptionHandling(exceptionHandling -> exceptionHandling
-						.authenticationEntryPoint(authenticationEntryPoint) // 인증이 수행되지 않았을 때 호출되는 핸들러
-						.accessDeniedHandler(accessDeniedHandler)); // 접근 권한이 없을 때 호출되는 핸들러
+			// ✅ 예외 처리 설정
+			.exceptionHandling(exceptionHandling -> exceptionHandling
+				.authenticationEntryPoint(authenticationEntryPoint) // 인증이 수행되지 않았을 때 호출되는 핸들러
+				.accessDeniedHandler(accessDeniedHandler)); // 접근 권한이 없을 때 호출되는 핸들러
 
 		return http.build();
 	}
@@ -111,7 +124,7 @@ public class SecurityConfig {
 		CorsConfiguration configuration = new CorsConfiguration();
 		// 허용할 오리진 설정
 		configuration.setAllowedOrigins(
-				List.of(frontendConfig.getFrontendUrl())); // 프론트 엔드
+			List.of(frontendConfig.getFrontendUrl())); // 프론트 엔드
 		// 허용할 HTTP 메서드 설정
 		configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 프론트 엔드 허용 메서드
 		// 자격 증명 허용 설정
@@ -120,12 +133,11 @@ public class SecurityConfig {
 		configuration.setAllowedHeaders(Collections.singletonList("*"));
 
 		configuration.setExposedHeaders(
-				List.of("Set-Cookie"));
+			List.of("Set-Cookie"));
 
 		// CORS 설정을 소스에 등록
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-
 }
---
+++ src/main/java/ssammudan/cotree/global/config/security/oauth/handler/OAuth2FailureHandler.java
@@ -0,0 +1,41 @@
+package ssammudan.cotree.global.config.security.oauth.handler;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ssammudan.cotree.infra.frontend.FrontendConfig;
+
+/**
+ * PackageName : ssammudan.cotree.global.config.security.oauth.handler
+ * FileName    : OAuth2FailureHandler
+ * Author      : hc
+ * Date        : 25. 4. 2.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 4. 2.     hc               Initial creation
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+	private final FrontendConfig frontendConfig;
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+		AuthenticationException exception) throws IOException, ServletException {
+		log.error("OAuth2 인증 실패 : {}", exception.getMessage());
+		String frontendUrl = frontendConfig.getFrontendUrl();
+
+		response.sendRedirect(frontendUrl + "/login/callback?error=true");
+	}
+}
