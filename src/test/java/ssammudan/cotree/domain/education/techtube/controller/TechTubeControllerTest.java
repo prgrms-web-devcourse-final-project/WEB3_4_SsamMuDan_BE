ae366d556c31dbff91f240a12ee013ba672587bd
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-04-04 15:30:45 +0900

    test: TechTube 컨트롤러 테스트 추가

--- src/test/java/ssammudan/cotree/domain/education/techtube/controller/TechTubeControllerTest.java
+++ src/test/java/ssammudan/cotree/domain/education/techtube/controller/TechTubeControllerTest.java
@@ -1,10 +1,12 @@
-package ssammudan.cotree.domain.education.techbook.controller;
+package ssammudan.cotree.domain.education.techtube.controller;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.time.Duration;
 import java.util.List;
 
 import javax.validation.constraints.Max;
@@ -25,73 +27,72 @@ import net.jqwik.api.Arbitraries;
 
 import autoparams.AutoSource;
 import autoparams.Repeat;
-import ssammudan.cotree.domain.education.techbook.dto.TechBookResponse;
+import ssammudan.cotree.domain.education.techtube.dto.TechTubeResponse;
 import ssammudan.cotree.global.response.BaseResponse;
 import ssammudan.cotree.global.response.ErrorCode;
 import ssammudan.cotree.global.response.PageResponse;
 import ssammudan.cotree.global.response.SuccessCode;
 import ssammudan.cotree.integration.WebMvcTestSupporter;
-import ssammudan.cotree.model.education.techbook.techbook.entity.TechBook;
+import ssammudan.cotree.model.education.techtube.techtube.entity.TechTube;
 
 /**
- * PackageName : ssammudan.cotree.domain.education.techbook.controller
- * FileName    : TechBookControllerTest
+ * PackageName : ssammudan.cotree.domain.education.techtube.controller
+ * FileName    : TechTubeControllerTest
  * Author      : loadingKKamo21
- * Date        : 25. 3. 31.
- * Description : TechBook 컨트롤러 테스트
+ * Date        : 25. 4. 4.
+ * Description : TechTube 컨트롤러 테스트
  * =====================================================================================================================
  * DATE          AUTHOR               NOTE
  * ---------------------------------------------------------------------------------------------------------------------
- * 25. 3. 31.    loadingKKamo21       Initial creation
- * 25. 4. 1.     loadingKKamo21       GET: /api/v1/education/techbook 테스트 추가
+ * 25. 4. 4.     loadingKKamo21       Initial creation
  */
-class TechBookControllerTest extends WebMvcTestSupporter {
+class TechTubeControllerTest extends WebMvcTestSupporter {
 
-	private TechBookResponse.Detail createTechBookResponseDetail(final Long id) {
-		return TechBookResponse.Detail.from(entityFixtureMonkey.giveMeBuilder(TechBook.class)
+	private TechTubeResponse.Detail createTechTubeResponseDetail(final Long id) {
+		return TechTubeResponse.Detail.from(entityFixtureMonkey.giveMeBuilder(TechTube.class)
 			.set("id", id)
 			.set("totalRating", Arbitraries.integers().greaterOrEqual(0))
 			.set("totalReviewCount", Arbitraries.integers().greaterOrEqual(0))
-			.set("techBookPage", Arbitraries.integers().greaterOrEqual(0))
+			.set("techTubeDuration", Arbitraries.longs().greaterOrEqual(0).map(Duration::ofSeconds))
 			.set("price", Arbitraries.integers().greaterOrEqual(0))
 			.set("viewCount", Arbitraries.integers().greaterOrEqual(0))
 			.sample());
 	}
 
-	private List<TechBookResponse.ListInfo> createTechBookResponseListInfo(final int size) {
-		return entityFixtureMonkey.giveMeBuilder(TechBook.class)
+	private List<TechTubeResponse.ListInfo> createTechTubeResponseListInfo(final int size) {
+		return entityFixtureMonkey.giveMeBuilder(TechTube.class)
 			.set("totalRating", Arbitraries.integers().greaterOrEqual(0))
 			.set("totalReviewCount", Arbitraries.integers().greaterOrEqual(0))
-			.set("techBookPage", Arbitraries.integers().greaterOrEqual(0))
+			.set("techTubeDuration", Arbitraries.longs().greaterOrEqual(0).map(Duration::ofSeconds))
 			.set("price", Arbitraries.integers().greaterOrEqual(0))
 			.set("viewCount", Arbitraries.integers().greaterOrEqual(0))
 			.sampleList(size)
 			.stream()
-			.map(TechBookResponse.ListInfo::from)
+			.map(TechTubeResponse.ListInfo::from)
 			.toList();
 	}
 
 	@ParameterizedTest
 	@AutoSource
 	@Repeat(10)
-	@DisplayName("[Success] getTechBookById(): TechBook 단 건 조회")
-	void getTechBookById(@Min(1) @Max(Long.MAX_VALUE) final Long id) throws Exception {
+	@DisplayName("[Success] getTechTubeById(): TechTube 단 건 조회")
+	void getTechTubeById(@Min(1) @Max(Long.MAX_VALUE) final Long id) throws Exception {
 		//Given
-		TechBookResponse.Detail responseDto = createTechBookResponseDetail(id);
+		TechTubeResponse.Detail responseDto = createTechTubeResponseDetail(id);
 
-		when(techBookService.findTechBookById(id)).thenReturn(responseDto);
+		when(techTubeService.findTechTubeById(id)).thenReturn(responseDto);
 
 		//When
-		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techbook/{id}/info", id));
+		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techtube/{id}/info", id));
 
 		//Then
-		BaseResponse<TechBookResponse.Detail> baseResponse = BaseResponse.success(
-			SuccessCode.TECH_BOOK_READ_SUCCESS, responseDto
+		BaseResponse<TechTubeResponse.Detail> baseResponse = BaseResponse.success(
+			SuccessCode.TECH_TUBE_READ_SUCCESS, responseDto
 		);
 		String responseBody = objectMapper.writeValueAsString(baseResponse);
 
-		resultActions.andExpect(handler().handlerType(TechBookController.class))
-			.andExpect(handler().methodName("getTechBookById"))
+		resultActions.andExpect(handler().handlerType(TechTubeController.class))
+			.andExpect(handler().methodName("getTechTubeById"))
 			.andExpect(status().isOk())
 			.andExpect(content().json(responseBody))
 			.andDo(print());
@@ -100,19 +101,19 @@ class TechBookControllerTest extends WebMvcTestSupporter {
 	@ParameterizedTest
 	@AutoSource
 	@Repeat(10)
-	@DisplayName("[Invalid] getTechBookById_invalidId(): TechBook 단 건 조회, Invalid ID")
-	void getTechBookById_unknownId(@Min(Long.MIN_VALUE) @Max(0) final Long invalidId) throws Exception {
+	@DisplayName("[Invalid] getTechTubeById_invalidId(): TechTube 단 건 조회, Invalid ID")
+	void getTechTubeById_unknownId(@Min(Long.MIN_VALUE) @Max(0) final Long invalidId) throws Exception {
 		//Given
 
 		//When
-		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techbook/{id}/info", invalidId));
+		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techtube/{id}/info", invalidId));
 
 		//Then
 		BaseResponse<Void> baseResponse = BaseResponse.fail(ErrorCode.INVALID_INPUT_VALUE);
 		String responseBody = objectMapper.writeValueAsString(baseResponse);
 
-		resultActions.andExpect(handler().handlerType(TechBookController.class))
-			.andExpect(handler().methodName("getTechBookById"))
+		resultActions.andExpect(handler().handlerType(TechTubeController.class))
+			.andExpect(handler().methodName("getTechTubeById"))
 			.andExpect(status().isBadRequest())
 			.andExpect(content().json(responseBody))
 			.andDo(print());
@@ -120,17 +121,17 @@ class TechBookControllerTest extends WebMvcTestSupporter {
 
 	//@RepeatedTest(10)
 	@Test
-	@DisplayName("[Success] getTechBooks(): TechBook 다 건 조회, 페이징 적용")
+	@DisplayName("[Success] getTechTubes(): TechTube 다 건 조회, 페이징 적용")
 	void getTechBooks() throws Exception {
 		//Given
-		List<TechBookResponse.ListInfo> content = createTechBookResponseListInfo(50);
-		PageResponse<TechBookResponse.ListInfo> resopnseDto = PageResponse.of(new PageImpl<>(
+		List<TechTubeResponse.ListInfo> content = createTechTubeResponseListInfo(50);
+		PageResponse<TechTubeResponse.ListInfo> resopnseDto = PageResponse.of(new PageImpl<>(
 			content,
 			PageRequest.of(0, 16, Sort.Direction.DESC, "createdAt"),
 			content.size()
 		));
 
-		when(techBookService.findAllTechBooks(anyString(), any(Pageable.class))).thenReturn(resopnseDto);
+		when(techTubeService.findAllTechTubes(anyString(), any(Pageable.class))).thenReturn(resopnseDto);
 
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 		params.add("page", "0");
@@ -139,16 +140,16 @@ class TechBookControllerTest extends WebMvcTestSupporter {
 		params.add("keyword", dtoFixtureMonkey.giveMeOne(String.class));
 
 		//When
-		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techbook").params(params));
+		ResultActions resultActions = mockMvc.perform(get("/api/v1/education/techtube").params(params));
 
 		//Then
-		BaseResponse<PageResponse<TechBookResponse.ListInfo>> baseResponse = BaseResponse.success(
-			SuccessCode.TECH_BOOK_LIST_FIND_SUCCESS, resopnseDto
+		BaseResponse<PageResponse<TechTubeResponse.ListInfo>> baseResponse = BaseResponse.success(
+			SuccessCode.TECH_TUBE_LIST_FIND_SUCCESS, resopnseDto
 		);
 		String responseBody = objectMapper.writeValueAsString(baseResponse);
 
-		resultActions.andExpect(handler().handlerType(TechBookController.class))
-			.andExpect(handler().methodName("getTechBooks"))
+		resultActions.andExpect(handler().handlerType(TechTubeController.class))
+			.andExpect(handler().methodName("getTechTubes"))
 			.andExpect(status().isOk())
 			.andExpect(content().json(responseBody))
 			.andDo(print());
