3176ec16275148ffb65bd386c6a050c557f3b815
sobaman <sobaman522@gmail.com>
2025-04-04 16:03:58 +0900

    feat: sms 전화번호 인증 관련 dto 생성
    - sms 전화번호 발송 request
    - sms 전화번호 인증 관련 request

---
+++ src/main/java/ssammudan/cotree/domain/member/dto/signup/SmsAuthenticationRequest.java
@@ -0,0 +1,24 @@
+package ssammudan.cotree.domain.member.dto.signup;
+
+import jakarta.validation.constraints.Pattern;
+
+/**
+ * PackageName : ssammudan.cotree.domain.member.dto.signup
+ * FileName    : SmsAuthenticationRequest
+ * Author      : kwak
+ * Date        : 2025. 4. 4.
+ * Description : 
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 4. 4.     kwak               Initial creation
+ */
+public record SmsAuthenticationRequest(
+
+	@Pattern(
+		regexp = "^01[016789]\\d{3,4}\\d{4}$",
+		message = "휴대폰 번호 형식이 올바르지 않습니다."
+	)
+	String receiverNumber
+) {
+}
