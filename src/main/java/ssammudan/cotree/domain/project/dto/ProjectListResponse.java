af2fc21486be1da19a93caabd08e3d353dec2c39
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-07 02:00:29 +0900

    refactor: builder 패턴 제거

--- src/main/java/ssammudan/cotree/domain/project/dto/ProjectListResponse.java
+++ src/main/java/ssammudan/cotree/domain/project/dto/ProjectListResponse.java
@@ -3,11 +3,6 @@ package ssammudan.cotree.domain.project.dto;
 import java.time.LocalDate;
 import java.util.List;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import ssammudan.cotree.model.member.member.entity.Member;
-import ssammudan.cotree.model.project.project.entity.Project;
-
 /**
  * PackageName : ssammudan.cotree.domain.project.dto
  * FileName    : HotProjectResponse
@@ -19,7 +14,6 @@ import ssammudan.cotree.model.project.project.entity.Project;
  * ---------------------------------------------------------------------------------------------------------------------
  * 2025. 4. 3.     sangxxjin               Initial creation
  */
-@Builder(access = AccessLevel.PRIVATE)
 public record ProjectListResponse(
 	Long id,
 	String title,
@@ -35,24 +29,4 @@ public record ProjectListResponse(
 	String username,
 	String userProfileImageUrl
 ) {
-	public static ProjectListResponse from(Project project, List<String> techStacksImageUrl, long likeCount,
-		Member member,
-		int recruitmentCount) {
-		return ProjectListResponse.builder()
-			.id(project.getId())
-			.title(project.getTitle())
-			.description(project.getDescription().length() > 30 ? project.getDescription().substring(0, 30) + "..." :
-				project.getDescription())
-			.imageUrl(project.getProjectImageUrl())
-			.viewCount(project.getViewCount())
-			.likeCount(likeCount)
-			.recruitmentCount(recruitmentCount)
-			.isOpen(project.getIsOpen())
-			.startDate(project.getStartDate())
-			.endDate(project.getEndDate())
-			.techStacksImageUrl(techStacksImageUrl)
-			.username(member.getUsername())
-			.userProfileImageUrl(member.getProfileImageUrl())
-			.build();
-	}
 }
