7a5734121ef3d4ff396dca1a794b7b2f0074e245
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-04-07 22:04:55 +0900

    feat: PaymentClient 인터페이스 및 Toss API 클라이언트 추가

---
+++ src/main/java/ssammudan/cotree/infra/payment/PaymentClient.java
@@ -0,0 +1,15 @@
+package ssammudan.cotree.infra.payment;
+
+/**
+ * PackageName : ssammudan.cotree.infra.payment
+ * FileName    : PaymentClient
+ * Author      : loadingKKamo21
+ * Date        : 25. 4. 7.
+ * Description : 결제 API 클라이언트
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 4. 7.     loadingKKamo21       Initial creation
+ */
+public interface PaymentClient {
+}
