65f371acb4e0d6529a0e3b25b7a7e067858b66a6
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-04-07 22:12:02 +0900

    feat: PaymentVerificationService 추가
    - 결제 승인 요청 시 사전에 서버에 저장된 결제 정보와 일치 여부를 검증
      - 이는 클라이언트가 결제 API를 호출하면서 보낸 정보와 서버가 갖고 있는 결제 정보를
        비교/검증하여 주문 상품과 결제 금액 등 결제 정보의 손실/변경을 방지하기 위함

---
+++ src/main/java/ssammudan/cotree/domain/payment/service/PaymentVerificationService.java
@@ -0,0 +1,55 @@
+package ssammudan.cotree.domain.payment.service;
+
+import java.util.Objects;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import ssammudan.cotree.domain.payment.dto.PaymentRequest;
+import ssammudan.cotree.domain.payment.dto.PrePaymentValue;
+import ssammudan.cotree.global.error.GlobalException;
+import ssammudan.cotree.global.response.ErrorCode;
+
+/**
+ * PackageName : ssammudan.cotree.domain.payment.service
+ * FileName    : PaymentVerificationService
+ * Author      : loadingKKamo21
+ * Date        : 25. 4. 7.
+ * Description : 서버에 저장된 사전 결제 정보와 클라이언트의 결제 요청 정보를 비교/검증하는 서비스
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 25. 4. 7.     loadingKKamo21       Initial creation
+ */
+@Service
+@RequiredArgsConstructor
+public class PaymentVerificationService {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	/**
+	 * 사전 저장 결제 정보 검증
+	 *
+	 * @param requestDto - 토스페이먼츠 결제 요청 DTO
+	 * @param memberId   - 회원 ID
+	 * @return 검증 완료 객체
+	 */
+	PrePaymentValue verify(final PaymentRequest.TossPayments requestDto, final String memberId) {
+		String redisKey = "prepay:%s".formatted(requestDto.orderId());
+		PrePaymentValue value = (PrePaymentValue)redisTemplate.opsForValue().get(redisKey);
+
+		if (value == null) {
+			throw new GlobalException(ErrorCode.PAYMENT_EXPIRED_PREPAYMENT);
+		}
+
+		if (!value.memberId().equals(memberId)
+			|| !Objects.equals(value.info().orderId(), requestDto.orderId())
+			|| value.info().amount() != requestDto.amount()) {
+			throw new GlobalException(ErrorCode.PAYMENT_REQUEST_INVALID);
+		}
+
+		return value;
+	}
+
+}
