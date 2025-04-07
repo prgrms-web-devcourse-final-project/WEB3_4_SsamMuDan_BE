530e2051fd87ce0d7c3013bc3d286404bb4f8a9e
k-haechan <gibbm1127@naver.com>
2025-04-07 17:15:06 +0900

    fix: 인증 확인 경로 접근 권한 설정(permitAll)

--- src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
+++ src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
@@ -72,6 +72,7 @@ public class SecurityConfig {
 				.requestMatchers(POST, "/api/v1/member/signup").permitAll()
 				.requestMatchers(POST, "/api/v1/member/signin").permitAll()
 				.requestMatchers(POST, "/api/v1/member/signup/phone").permitAll()
+				.requestMatchers(POST, "/api/v1/member/signup/phone/verify").permitAll()
 
 				// Email Domain
 				.requestMatchers(POST, "/api/v1/email/**").permitAll()
