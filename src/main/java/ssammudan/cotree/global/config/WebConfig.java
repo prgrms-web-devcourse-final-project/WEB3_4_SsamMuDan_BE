a5b94482c51e0af30066adbae43db72b48d53694
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-04-08 10:41:40 +0900

    fix: WebClient 빈 등록 클래스 삭제, RestTemplate 빈 등록 클래스 추가

--- src/main/java/ssammudan/cotree/global/config/WebConfig.java
+++ src/main/java/ssammudan/cotree/global/config/WebConfig.java
@@ -1,30 +1,36 @@
 package ssammudan.cotree.global.config;
 
+import java.time.Duration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * PackageName : ssammudan.cotree.global.config
- * FileName    : WebClientConfig
+ * FileName    : WebConfig
  * Author      : loadingKKamo21
- * Date        : 25. 4. 5.
- * Description : WebClient 빈 등록
+ * Date        : 25. 4. 8.
+ * Description : RestTemplate 빈 등록
  * =====================================================================================================================
  * DATE          AUTHOR               NOTE
  * ---------------------------------------------------------------------------------------------------------------------
- * 25. 4. 5.     loadingKKamo21       Initial creation
+ * 25. 4. 8.     loadingKKamo21       Initial creation
  */
 @Configuration
-public class WebClientConfig {
+public class WebConfig {
 
 	@Bean
-	public WebClient.Builder webClientBuilder() {
-		return WebClient.builder()
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder
 			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.connectTimeout(Duration.ofSeconds(3))    //서버 연결 시도 최대 시간
+			.readTimeout(Duration.ofSeconds(5))    //데이터 응답 대기 최대 시간
+			.build();
 	}
 
 }
