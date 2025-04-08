87e4650a217bf0b956682144f8d505b53a45c594
loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
2025-04-08 12:17:43 +0900

    refactor: PaymentController Swagger 설정 추가

--- src/main/java/ssammudan/cotree/domain/payment/controller/PaymentController.java
+++ src/main/java/ssammudan/cotree/domain/payment/controller/PaymentController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
@@ -36,11 +39,14 @@ import ssammudan.cotree.global.response.SuccessCode;
 @RestController
 @RequestMapping("/api/v1/payment/")
 @RequiredArgsConstructor
+@Tag(name = "Payment Controller", description = "Payment API")
 public class PaymentController {
 
 	private final PaymentService paymentService;
 
 	@PostMapping("/prepare")
+	@Operation(summary = "결제 전 결제 정보 저장 및 주문번호 발급", description = "결제 전 주문 정보를 저장하고 주문번호를 발급")
+	@ApiResponse(responseCode = "200", description = "정보 저장 및 주문번호 발급 성공")
 	public BaseResponse<PaymentResponse.PrePaymentInfo> preparePayment(
 		@RequestBody @Valid final PaymentRequest.PrePayment requestDto,
 		@AuthenticationPrincipal final UserDetails userDetails
@@ -52,6 +58,8 @@ public class PaymentController {
 	}
 
 	@GetMapping("/confirm")
+	@Operation(summary = "결제 승인 API 호출", description = "결제 API로부터 받은 정보로 서버에서 결제 정보 검증 후 결제 승인 API 호출")
+	@ApiResponse(responseCode = "200", description = "결제 성공")
 	public BaseResponse<PaymentResponse.Detail> confirmPayment(
 		@RequestParam("orderId") String orderId,
 		@RequestParam("paymentKey") String paymentKey,
