1ddd721e7140a498eccb9ffe06a880b4ebc9d014
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-09 15:33:59 +0900

    feat: UpdateProjectPositionRequest

---
+++ src/main/java/ssammudan/cotree/domain/project/dto/UpdateProjectPositionRequest.java
@@ -0,0 +1,18 @@
+package ssammudan.cotree.domain.project.dto;
+
+/**
+ * PackageName : ssammudan.cotree.domain.project.dto
+ * FileName    : UpdateProjectPositionRequest
+ * Author      : sangxxjin
+ * Date        : 2025. 4. 9.
+ * Description : UpdateProjectPositionRequest
+ * =====================================================================================================================
+ * DATE          AUTHOR               NOTE
+ * ---------------------------------------------------------------------------------------------------------------------
+ * 2025. 4. 9.     sangxxjin               Initial creation
+ */
+public record UpdateProjectPositionRequest(
+	Long projectDevPositionId,
+	Integer amount
+) {
+}
