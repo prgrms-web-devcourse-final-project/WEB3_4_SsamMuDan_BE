291b5d323a78ce5fa3a94303f1a0acef80c0f544
sangxxjin <118094734+sangxxjin@users.noreply.github.com>
2025-04-09 11:30:40 +0900

    feat: 프로젝트 참가자 목록조회 SuccessCode

--- src/main/java/ssammudan/cotree/global/response/SuccessCode.java
+++ src/main/java/ssammudan/cotree/global/response/SuccessCode.java
@@ -45,6 +45,7 @@ public enum SuccessCode {
 	PROJECT_FETCH_SUCCESS(HttpStatus.OK, "200", "프로젝트 상세 조회 성공"),
 	PROJECT_STATUS_UPDATE_SUCCESS(HttpStatus.OK, "200", "프로젝트 모집 상태 변경 성공"),
 	PROJECT_APPLY_SUCCESS(HttpStatus.OK, "200", "프로젝트 참가 신청 성공"),
+	PROJECT_MEMBERSHIP_LIST_RETRIEVED(HttpStatus.OK, "200", "프로젝트 참가자 목록 조회 성공"),
 
 	//Community
 	COMMUNITY_BOARD_CREATE_SUCCESS(HttpStatus.CREATED, "201", "글 작성 성공"),
