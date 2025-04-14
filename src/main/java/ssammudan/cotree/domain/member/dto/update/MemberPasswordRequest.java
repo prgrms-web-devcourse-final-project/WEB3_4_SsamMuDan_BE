62496ce0ca12d8701f5e904654ac9835c6e46e69
k-haechan <gibbm1127@naver.com>
2025-04-14 14:52:12 +0900

    fix: 비밀번호 재설정 시 스프링 시큐리티 인증정보 불필요 - 에고 시큐리티 단에만 풀고 내부적인 로직은 확인을 안했네요ㅠㅠ - 이메일 인증을 완료한 회원의 정보는 redis에 담아놓아서 시큐리티의 인증정보 대신 redis로 인증확인을 합니다. - email을 요청으로 같이 받습니다.

--- src/main/java/ssammudan/cotree/domain/member/dto/update/MemberPasswordRequest.java
+++ src/main/java/ssammudan/cotree/domain/member/dto/update/MemberPasswordRequest.java
@@ -1,6 +1,7 @@
 package ssammudan.cotree.domain.member.dto.update;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 /**
@@ -15,6 +16,10 @@ import jakarta.validation.constraints.NotBlank;
  * 25. 4. 14.     hc               Initial creation
  */
 public record MemberPasswordRequest(
+	@NotBlank(message = "이메일을 입력해주세요.")
+	@Email(message = "이메일 형식이 올바르지 않습니다.")
+	String email,
+
 	@NotBlank(message = "비밀번호를 입력해주세요.")
 	@Schema(description = "사용자 비밀번호", examples = "password123@@")
 	String password
