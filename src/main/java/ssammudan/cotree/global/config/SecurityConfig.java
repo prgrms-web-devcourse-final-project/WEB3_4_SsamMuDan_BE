71292963a704a9129441a3b2ba388251dee558bd
Baegwa <160799011+KangBaekGwa@users.noreply.github.com>
2025-04-25 11:52:39 +0900

    [FEAT] : Health Check End-point 라이트 버전 추가 (#257)
    * 2025-04-01 Main merge (#33)
    
    * [FEAT] S3 공통 업로드 구현 (#IS-1) (#19)
    
    * feat : NoResourceFoundException 핸들링 추가
    
    * feat : MethodArgumentTypeMismatchException 핸들링 추가
    
    * feat : S3 업로드 등록
    - AWS S3 접근 인증 방법 AWS CLI 로 통일
    - `S3Directory` 및 Member UUID 별 파일 등록
    - Directory 별, 멀티 파일인지, 싱글 파일인지 여부 선택
    - End-point 추가
    
    * chore : build_test 안돌아서 신규 커밋
    
    * fix : 테스트 spring context, s3 관련 bean 등록하지 않도록 mocking 진행
    
    * fix : S3 Bean 관련 SpringBootTest 에서 Mocking 진행
    
    * refactor : OAuth_Provider 테이블 삭제 처리 (#30)
    
    * feat: #ET-3 TechBook 페이징 목록 조회 기능 구현 (#25)
    
    * fix: TechBookRequest Valid message 삭제
    
    - messages.properties 적용 이전 고려 삭제
    
    * feat: 검색어로 TechBook 페이징 목록 조회 추가
    
    * test: DataJpaTestSupporter 수정 및 테스트 추가
    
    - TechBookRepository 테스트 추가
    - DataJpaTestSupporter 수정에 따른 EducationLevelRepository 테스트 수정
    
    * feat: TechBookService findAllTechBooksByKeyword() 추가
    
    - TechBookResponse.ListInfo 추가: 목록 조회 시 필요한 필드만 포함
    
    * test: TechBookService 테스트 추가
    
    * feat: TechBookController findAllTechBooks() 추가
    
    - SuccessCode 메시지 추가
    
    * test: TechBookController 테스트 추가
    
    * test: 반복 테스트(RepeatedTest) 고려 setup() 명시
    
    ---------
    
    Co-authored-by: loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
    
    * [FEAT] #CB-2 커뮤니티 글 목록 조회 기능 구현 (#26)
    
    * feat : 임시 커밋
    
    * chore : 쓸모없는 주석 삭제
    
    * feat : 게시글 조회 기능 구현
    - queryDSL 적용
    - count 조회 현재, 서브쿼리로 진행
    - 썸네일 이미지 추출 적용되지 않음
    
    * feat : member, community, like, comment `dev` 프로파일용, 더미 데이터 생성
    
    * [FIX] 인스턴스 구성 변경 및 스크립트를 이용한 기본 환경 셋팅 (#24)
    
    * feat: 보안과 관련된 terraform파일 ignore처리
    
    * feat: AWS 인프라 설정 (VPC, 서브넷, 보안 그룹, 출력값 정의)
    
    * feat: EC2-1 서버 https 추가
    
    * feat: 보안그룹 이름 설정
    
    * feat: api-server에 탄력적 IP 자동 재연결 설정
    
    * feat: 볼륨, 용량 설정
    
    * fix: 볼륨 추가에서 루트 디스크 설정으로 변경
    
    * feat: SSH 연결을 위한 키페어 설정
    
    * feat: server environment setting
    
    * fix: terraform.tfvars -> secrets.tf로 민감 정보 변경
    
    * fix: 인스턴스 1개로 변경, 구조 변경, npm(nginx proxy manager),docker, redis 등 미리 설치
    
    ---------
    
    Co-authored-by: loadingKKamo21 <90470901+loadingKKamo21@users.noreply.github.com>
    Co-authored-by: loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
    Co-authored-by: sangxxjin <118094734+sangxxjin@users.noreply.github.com>
    
    * Update init.sh
    
    * feat : health check end-point 추가
    - 가벼운 버전
    
    * fix : ha_proxy health check end-point 변경
    - 기존 : /actuator/health
    - 변경 : /health
    
    ---------
    
    Co-authored-by: loadingKKamo21 <90470901+loadingKKamo21@users.noreply.github.com>
    Co-authored-by: loadingKKamo21 <loadingKKamo21@users.noreply.github.com>
    Co-authored-by: sangxxjin <118094734+sangxxjin@users.noreply.github.com>
    Co-authored-by: sobaman <122892573+sobaman@users.noreply.github.com>

--- src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
+++ src/main/java/ssammudan/cotree/global/config/SecurityConfig.java
@@ -66,6 +66,7 @@ public class SecurityConfig {
 				.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
 					"/swagger-ui.html").permitAll()
 				.requestMatchers(GET, "/actuator/**").permitAll()
+				.requestMatchers(GET, "/health").permitAll()
 
 				// MEMBER Domain
 				.requestMatchers(POST, "/api/v1/member/signup").permitAll()
